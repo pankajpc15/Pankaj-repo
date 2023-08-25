853ed7a7939ff2b30df8982dda3b0cecb919fc02
pankajpc15 <pankaj21003@gmail.com>
2023-08-26 00:35:46 +0530

    1st

---
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/hello")
+public class MyController {
+	
+	@GetMapping(path = "/greet")
+	
+	public String hello() {
+		return "server is running fine";
+	}
+	
+	
+	
+
+}
